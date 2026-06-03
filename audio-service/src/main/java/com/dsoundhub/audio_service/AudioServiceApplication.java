47e42a947ce5744e08b1bb39a6096019faf53649
Dhea Firmasari <walkerdheafirm@gmail.com>
2026-06-03 15:07:54 +0700

    chore: initial project structure auth-service and audio-service

---
+++ audio-service/src/main/java/com/dsoundhub/audio_service/AudioServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dsoundhub.audio_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudioServiceApplication.class, args);
+	}
+
+}
