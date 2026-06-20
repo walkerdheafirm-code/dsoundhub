85eae349c2d22bd6f93f6518f6a31d90319a25aa
Dhea Firmasari <walkerdheafirm@gmail.com>
2026-06-20 23:51:37 +0700

    fix(security): load JWT secret from .env using dotenv-java

--- audio-service/src/main/java/com/dsoundhub/audio_service/AudioServiceApplication.java
+++ audio-service/src/main/java/com/dsoundhub/audio_service/AudioServiceApplication.java
@@ -3,10 +3,17 @@ package com.dsoundhub.audio_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class AudioServiceApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.ignoreIfMissing()
+				.load();
+
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(AudioServiceApplication.class, args);
 	}
 
